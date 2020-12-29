3d9e1cc14347bec4bf950711a188de1d97fb63aa
Nick Rosier <github@bunbun.be>
2020-12-29 16:13:26 +0100

    Update received + clear screen

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    soundExpression.giggle.play()
     if (receivedNumber == 0) {
         basic.showString("A")
     } else {
         basic.showString("B")
     }
+    basic.pause(2000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
