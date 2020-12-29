e2808e479c2415d0b848e13f7fc09c847ea9269e
Nick Rosier <github@bunbun.be>
2020-12-29 16:09:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showString("A")
+    } else {
+        basic.showString("B")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
